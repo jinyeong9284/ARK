bc40a0cae98c90d7c7f930f9aa6f82dc9b4ffdd9
jinyeong9284 <jjy062512@naver.com>
2024-06-18 17:18:22 +0900

    포트폴리오

---
+++ index.js
@@ -0,0 +1,27 @@
+let image = document.getElementById("image");
+let images = [
+  "./assets/images/ark2.png",
+  "./assets/images/ark3.png",
+  "./assets/images/ark4.png",
+  "./assets/images/ark5.png",
+  "./assets/images/ark6.png",
+  "./assets/images/ark7.png",
+];
+let intervalId = setInterval(function () {
+  let random = Math.floor(Math.random() * images.length);
+  image.src = images[random];
+}, 200);
+
+// Pause interval on image hover
+image.addEventListener("mouseenter", function () {
+  clearInterval(intervalId); // Clear the interval when hovering
+});
+
+// Resume interval when mouse leaves the image
+image.addEventListener("mouseleave", function () {
+  // Restart the interval (if needed)
+  intervalId = setInterval(function () {
+    let random = Math.floor(Math.random() * images.length);
+    image.src = images[random];
+  }, 200);
+});
